dceaedaa97cfa3ccec1274a3f8db273b9d7b6785
Jeremy Yallop <yallop@gmail.com>
2017-01-28 13:32:49 +0000

    Add checked signatures to the HOAS example.

--- examples/hoas.f
+++ examples/hoas.f
@@ -8,16 +8,16 @@ ExpDict = λE::*⇒*.∀A::*.
 
 
 # term constructors
-# abs : ∀A::*.∀B::*.∀E::*⇒*.ExpDict E → (E A → E B) → E (A → B)
-abs = ΛA::*.ΛB::*.ΛE::*⇒*.λdict:ExpDict E.λf:E A → E B.
+abs : ∀A::*.∀B::*.∀E::*⇒*.ExpDict E → (E A → E B) → E (A → B)
+  = ΛA::*.ΛB::*.ΛE::*⇒*.λdict:ExpDict E.λf:E A → E B.
       dict  [∀A::*.∀B::*.(E A → E B) → E (A → B)]
         (λabs:∀A::*.∀B::*.(E A → E B) → E (A → B).
          λapp:∀A::*.∀B::*.E (A → B) → (E A → E B).
          abs) [A] [B] f
 ;
 
-# app : ∀A::*.∀B::*.Exp (A → B) → (Exp A → Exp B)
-app = ΛA::*.ΛB::*.ΛE::*⇒*.λdict:ExpDict E.λf:E (A → B).λp:E A.
+app : ∀A.∀B.∀E::*⇒*.ExpDict E → E (A → B) → E A → E B
+  = ΛA::*.ΛB::*.ΛE::*⇒*.λdict:ExpDict E.λf:E (A → B).λp:E A.
       dict  [∀A::*.∀B::*.E (A → B) → (E A → E B)]
         (λabs:∀A::*.∀B::*.(E A → E B) → E (A → B).
          λapp:∀A::*.∀B::*.E (A → B) → (E A → E B).
@@ -29,32 +29,32 @@ Expression = λA::*.(∀E::*⇒*.ExpDict E → E A);
 
 # Evaluator: interpret lambda terms as functions
 
-# evalDict : ExpDict (λA::*.A)
-evalDict = ΛA::*.
+evalDict : ExpDict (λA::*.A)
+  = ΛA::*.
      λk:(∀A::*.∀B::*.(A → B) → (A → B)) →
         (∀A::*.∀B::*.(A → B) → (A → B)) →
         A.
         k (ΛA::*.ΛB::*.λf:A→B.f) (ΛA::*.ΛB::*.λf:A→B.f)
 ;
       
-# eval : ∀A::*.Expression A → A
-eval = ΛA::*.λe:Expression A.e [λA::*.A] evalDict;
+eval : ∀A::*.Expression A → A
+  = ΛA::*.λe:Expression A.e [λA::*.A] evalDict;
 
 # natural numbers
 Nat = ∀A::*.A → (A → A) → A;
 
-# succ : Nat
-zero = ΛA::*.λz:A.λs:A → A.z;
+zero : Nat
+  = ΛA::*.λz:A.λs:A → A.z;
 
-# succ : Nat → Nat
-succ = λn:Nat.ΛA::*.λz:A.λs:A → A.s (n [A] z s);
+succ : Nat → Nat
+  = λn:Nat.ΛA::*.λz:A.λs:A → A.s (n [A] z s);
 
-# add : Nat → Nat → Nat
-add = λm:Nat.λn:Nat.m [Nat] n succ;
+add : Nat → Nat → Nat
+  = λm:Nat.λn:Nat.m [Nat] n succ;
 
 # Term size measurer: interpret lambda terms as integers representing their size
-# sizeDict : ExpDict (λA::*.Nat)
-sizeDict = ΛA::*.
+sizeDict : ExpDict (λA::*.Nat)
+  = ΛA::*.
      λk:(∀A::*.∀B::*.(Nat → Nat) → Nat) →
         (∀A::*.∀B::*.Nat → Nat → Nat) →
         A.
@@ -62,5 +62,5 @@ sizeDict = ΛA::*.
           (ΛA::*.ΛB::*.λf:Nat.λp:Nat.add f p)
 ;
 
-# size : ∀A::*.Expression A → Nat
-size = ΛA::*.λe:Expression A. e [λA::*.Nat] sizeDict;
+size : ∀A::*.Expression A → Nat
+  = ΛA::*.λe:Expression A. e [λA::*.Nat] sizeDict;
